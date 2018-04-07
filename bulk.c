6dc1dd09140ec69be88d70a2a8b231923accfc61
progvault <progvault@users.noreply.github.com>
2018-04-07 02:59:38 -0500

    Adding loopback test code

---
+++ bulk.c
@@ -0,0 +1,165 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <libusb-1.0/libusb.h>
+
+/* Change VENDOR_ID and PRODUCT_ID depending on device */
+#define VENDOR_ID   0x0471
+#define PRODUCT_ID  0x3630
+
+/* Define number of bytes to transfer */
+#define BYTES 1024*768*3 // bytes
+//#define BYTES 64*64      // bytes
+#define EP_SIZE 64       // bytes
+#define TIMEOUT 5*1000   // milliseconds
+
+/* Use a global variable to keep the device handle */
+static struct libusb_device_handle *devh = NULL;
+
+/* The Endpoint addresses are hard-coded.  You should use libusb -v to find
+ * the values corresponding to device
+ */
+static int ep_in_addr  = 0x82;
+static int ep_out_addr = 0x01;
+
+int write_chars(unsigned char * data, int length)
+{
+  /* To send a char to the device simply initiate a bulk_transfer to the Endpoint
+   * with the address ep_out_addr.
+   */
+  int actual_length;
+
+  int rc = libusb_bulk_transfer(devh, ep_out_addr, data, length, &actual_length, TIMEOUT);
+
+  if (rc < 0)
+  {
+    fprintf(stderr, "Error while sending char: %d\n", rc);
+    return -1;
+  }
+
+  return actual_length;
+}
+
+int read_chars(unsigned char * data, int length)
+{
+  /* To receive characters from the device initiate a bulk_transfer to the Entpoint
+   * with address ep_in_addr
+   */
+  int actual_length;
+
+  int rc = libusb_bulk_transfer(devh, ep_in_addr, data, length, &actual_length, TIMEOUT);
+
+  if (rc == LIBUSB_ERROR_TIMEOUT)
+  {
+    printf("timeout (%d)\n", actual_length);
+    return -1;
+  }
+  else if (rc < 0)
+  {
+    fprintf(stderr, "Error while waiting for char: %d\n", rc);
+    return -1;
+  }
+
+  return actual_length;
+}
+
+int main(int argc, char **argv)
+{
+  int rc;
+
+  /* Initialize libusb */
+  rc = libusb_init(NULL);
+
+  if (rc < 0)
+  {
+    fprintf(stderr, "Error Initializing libusb: %s\n", libusb_error_name(rc));
+    exit(1);
+  }
+
+  /* Set debugging output to max level */
+  libusb_set_debug(NULL, 3);
+
+  /* Look for a specific device and open it */
+  devh = libusb_open_device_with_vid_pid(NULL, VENDOR_ID, PRODUCT_ID);
+  if (!devh)
+  {
+    fprintf(stderr, "Error finding USB device\n");
+    goto out;
+  }
+
+  /* We can now start sending or receiving data to the device */
+  unsigned char buf[BYTES];
+  unsigned char rbuf[EP_SIZE];
+  int len;
+  int j;
+  int i;
+  int n;
+  int l;
+  int res;
+
+  // fill buffer
+  for (n = 0; n < BYTES; n++)
+  {
+    buf[n] = 0x00+n;
+  }
+
+  // loopback data, write-read back-to-back (~431 kB/sec), should be able to add twice the speed by interleaving
+  for (l = 0; l < BYTES/EP_SIZE; l++)
+  {
+    len = write_chars(buf+l*EP_SIZE, EP_SIZE);
+    len = read_chars(rbuf, EP_SIZE);
+    res = memcmp(rbuf, buf+l*EP_SIZE, sizeof(rbuf));
+    if (res != 0)
+      fprintf(stderr, "Miscompare: block %d\n", l);
+  }
+
+  // write data (~537.063 kB/sec), double-buffer should add twice the speed
+  //len = write_chars(buf, sizeof(buf));
+  //fprintf(stdout, "Sent: ");
+  //for (i = 0; i < len; i++)
+  //{
+  //  fprintf(stdout, "%02X ", buf[i]);
+  //}
+  //fprintf(stdout, "(%d bytes)", len);
+  //fprintf(stdout, "\n");
+
+  // read data
+  //len = read_chars(buf, sizeof(buf));
+  //fprintf(stdout, "Received: ");
+  //for (i = 0; i < len; i++)
+  //{
+  //  fprintf(stdout, "%02X ", buf[i]);
+  //}
+  //fprintf(stdout, "(%d bytes)", len);
+  //fprintf(stdout, "\n");
+
+  //for (j = 0; j < 65536; j++)
+  //{
+  //  buf[0] = j & 0xFF;
+  //  buf[1] = (j >> 8) & 0xFF;
+  //
+  //  len = write_chars(buf, sizeof(buf));
+  //  fprintf(stdout, "Sent: %d bytes.\n", len);
+  //
+  //  len = read_chars(buf, sizeof(buf));
+  //  fprintf(stdout, "Received: ");
+  //  for (i = 0; i < len; i++)
+  //  {
+  //    fprintf(stdout, "%02X ", buf[i]);
+  //  }
+  //  fprintf(stdout, "\n");
+  //}
+
+  //sleep(1);
+
+  libusb_release_interface(devh, 0);
+
+out:
+  if (devh)
+  {
+    libusb_close(devh);
+  }
+  libusb_exit(NULL);
+
+  return rc;
+}
---
+++ bulk_async.c
@@ -0,0 +1,144 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include </usr/include/libusb-1.0/libusb.h>
+
+/* Specify VENDOR_ID and PRODUCT_ID for device */
+#define VENDOR_ID   0x0471
+#define PRODUCT_ID  0x3630
+
+/* Define number of bytes to transfer */
+#define EP_SIZE 64                       // bytes
+#define TRANSFERS 1024*768*3/EP_SIZE     // number of transfers
+#define TIMEOUT 10*1000                  // milliseconds
+
+/* Use a global variable to keep the device handle */
+static struct libusb_device_handle *handle = NULL;
+
+/* count variables */
+unsigned int count = 0;
+unsigned int count_in = 0;
+unsigned int count_out = 0;
+
+/* The Endpoint addresses are hard-coded.  You should use libusb -v to find
+ * the values corresponding to device
+ */
+static int ep_in  = 0x82;
+static int ep_out = 0x01;
+
+/* Write and Read buffers */
+unsigned char wbuf[EP_SIZE*TRANSFERS];
+unsigned char wbuf_tmp[EP_SIZE*TRANSFERS];
+unsigned char rbuf[EP_SIZE*TRANSFERS];
+unsigned char rbuf_tmp[EP_SIZE*TRANSFERS];
+
+static void LIBUSB_CALL xfr_cb_out(struct libusb_transfer *transfer )
+{
+  memcpy(wbuf+count_out*EP_SIZE, transfer->buffer, EP_SIZE);
+}
+
+static void LIBUSB_CALL xfr_cb_in(struct libusb_transfer *transfer )
+{
+  int *completed = transfer->user_data;
+  memcpy(rbuf+count_in*EP_SIZE, transfer->buffer, EP_SIZE);
+
+  count_in++;  // one transfer complete
+  if (count_in < TRANSFERS)
+    *completed = 1;
+}
+
+int main(int argc, char **argv)
+{
+  const struct libusb_version *version;
+  int completed = 0;
+  size_t length = 64;
+  int n;
+  int m;
+  int rc;
+
+  /* Get libusb version */
+  version = libusb_get_version();
+  fprintf(stderr, "libusb version: %d.%d.%d.%d\n", version->major, version->minor, version->micro, version->nano);
+
+  /* Initialize libusb */
+  rc = libusb_init(NULL);
+  if (rc < 0)
+  {
+    fprintf(stderr, "Error Initializing libusb: %s\n", libusb_error_name(rc));
+    exit(1);
+  }
+
+  /* Set debugging output to max level */
+  libusb_set_debug(NULL, 3);
+
+  /* Look for a specific device and open it */
+  handle = libusb_open_device_with_vid_pid(NULL, VENDOR_ID, PRODUCT_ID);
+  if (!handle)
+  {
+    fprintf(stderr, "Error finding USB device\n");
+    goto out;
+  }
+
+  /* claim interface */
+  rc = libusb_claim_interface(handle, 0);
+  if (rc < 0)
+  {
+    fprintf(stderr, "Error claiming interface.\n");
+    goto out;
+  }
+
+  /* fill the buffer with incrementing data */
+  for (n = 0; n < TRANSFERS; n++)
+  {
+    for (m = 0; m < EP_SIZE; m++)
+    {
+      wbuf_tmp[m+n*EP_SIZE] = m+n;
+    }
+  }
+
+  struct libusb_transfer *transfer;
+  transfer = libusb_alloc_transfer(0);
+  libusb_fill_bulk_transfer(transfer, handle, ep_out, wbuf_tmp, EP_SIZE*TRANSFERS, xfr_cb_out, NULL, TIMEOUT);
+  libusb_submit_transfer(transfer);
+
+  transfer = libusb_alloc_transfer(0);
+  libusb_fill_bulk_transfer(transfer, handle, ep_in, rbuf_tmp, EP_SIZE*TRANSFERS, xfr_cb_in, &completed, TIMEOUT);
+  libusb_submit_transfer(transfer);
+
+  /* Handle Events */
+  while (!completed)
+  {
+    rc = libusb_handle_events_completed(NULL, &completed);
+    if (rc != LIBUSB_SUCCESS)
+    {
+      fprintf(stderr, "Transfer Error: %s\n", libusb_error_name(rc));
+      break;
+    }
+  }
+
+  fprintf(stderr, "completed\n");
+
+  int res;
+  res = memcmp(rbuf, wbuf, sizeof(wbuf));
+  if (res != 0)
+    fprintf(stderr, "miscompare\n");
+  else
+    fprintf(stderr, "success\n");
+
+  //* Release the interface */
+  libusb_release_interface(handle, 0);
+
+  /* Close the device handle */
+  if (handle)
+    libusb_close(handle);
+
+out:
+  if (handle)
+  {
+    libusb_close(handle);
+  }
+  libusb_exit(NULL);
+
+  return rc;
+}
